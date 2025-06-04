4aa9ba00e5930a3f97587a736cadc1d3d9b1c5cd
LAPTOP-UIUMCEAC\hp <i20169@e.hiroshima-cu.ac.jp>
2025-06-04 11:34:49 +0900

    wasdキーで移動に変更

---
+++ engine/player.py
@@ -0,0 +1,22 @@
+import pygame
+import config
+
+class Player:
+    def __init__(self, x, y):
+        self.image = pygame.image.load("assets/characters/hero.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.speed = config.plyer_speed
+     
+    def handle_keys(self,keys):
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+        
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
